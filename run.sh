fd3461c991ebf11f2ba9a4e941efd55fe2134367
Yazan Murhij <56399611+YznMur@users.noreply.github.com>
2023-03-28 14:36:46 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,5 @@
 ./Examples/Monocular/mono_kitti Vocabulary/superpoint_voc.yml.gz Examples/Monocular/KITTI04-12.yaml data_odometry_gray/dataset/sequences/05
 
+./Examples/Monocular/mono_kitti ./Vocabulary/superpoint_voc.yml.gz ./Examples/Monocular/KITTI00-02.yaml /home/Dataset/dataset/sequences/01
+
+
